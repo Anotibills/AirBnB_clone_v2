35a6f56b0b49dcdecb843d8e2a76201dad291fbf
Tolulope Abiola <anotibills@gmail.com>
2023-10-09 06:17:33 +0100

    clean 1

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,26 +1,37 @@
-# Nginx configuration file
-$nginx_conf = "server {
+# Configures a web server for deployment of web_static.
+
+$nginx_conf = "
+server {
     listen 80 default_server;
     listen [::]:80 default_server;
     add_header X-Served-By ${hostname};
-    root   /var/www/html;
-    index  index.html index.htm;
+    root /var/www/html;
+    index index.html index.htm;
+
     location /hbnb_static {
         alias /data/web_static/current;
         index index.html index.htm;
     }
+
     location /redirect_me {
         return 301 http://cuberule.com/;
     }
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}"
 
-# Define necessary directories and files
-file { ['/data', '/data/web_static', '/data/web_static/releases', '/data/web_static/shared']:
+    error_page 404 /404.html;
+
+    location /404 {
+        root /var/www/html;
+        internal;
+    }
+}
+"
+
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt',
+}
+
+file { ['/data', '/data/web_static', '/data/web_static/releases', '/data/web_static/releases/test', '/data/web_static/shared']:
   ensure => 'directory',
 }
 
@@ -29,42 +40,36 @@ file { '/data/web_static/releases/test/index.html':
   content => "Holberton School Puppet\n",
 }
 
-file { ['/var/www', '/var/www/html']:
-  ensure => 'directory',
-}
-
-file { ['/var/www/html/index.html', '/var/www/html/404.html']:
-  ensure  => 'present',
-  content => ["Holberton School Nginx\n", "Ceci n'est pas une page\n"],
-}
-
-# Create symbolic link for web_static
 file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test',
 }
 
-# Install Nginx package
-package { 'nginx':
-  ensure   => 'present',
-  provider => 'apt',
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/',
 }
 
-# Configure Nginx using the provided configuration
+file { ['/var/www', '/var/www/html']:
+  ensure => 'directory',
+}
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School Nginx\n",
+}
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n",
+}
+
 file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
   content => $nginx_conf,
 }
 
-# Restart Nginx
-exec { 'nginx restart':
+exec { 'nginx_restart':
   command => '/etc/init.d/nginx restart',
-  path    => ['/usr/bin', '/usr/local/bin', '/bin'],
-  require => File['/etc/nginx/sites-available/default'],
-}
-
-# Set ownership
-exec { 'chown -R ubuntu:ubuntu /data/':
-  path    => '/usr/bin:/usr/local/bin:/bin',
-  require => File['/data'],
+  refreshonly => true,
+  path => ['/sbin', '/usr/sbin', '/usr/local/sbin', '/bin', '/usr/bin'],
 }
